878b2c127d2e42a67cf378fe7ba59b00a9661a63
mdeslippe <mdeslippe76@gmail.com>
2021-01-31 13:57:02 -0500

    First commit

---
+++ server/src/main/java/app/easytracker/server/EasyTrackerServerApplication.java
@@ -0,0 +1,13 @@
+package app.easytracker.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyTrackerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyTrackerServerApplication.class, args);
+	}
+
+}
