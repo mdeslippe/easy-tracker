a3984a9b0a1fe55e142dcfbdcef8897f9263bfe8
Myles Deslippe <mdeslippe76@gmail.com>
2023-05-18 08:11:09 -0400

    #14: Added a migration to create the users table
    - Removed the temporary .gitkeep file from the migrations directory.
    - Added a migration to create the users table.

---
+++ migrations/20230518115455_create_users_table.up.sql
@@ -0,0 +1,19 @@
+-- Create the 'users' table.
+CREATE TABLE `users` (
+	`id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`account_created_at` TIMESTAMP NOT NULL DEFAULT current_timestamp(),
+	`password_reset_at` TIMESTAMP NOT NULL DEFAULT current_timestamp(),
+	`profile_picture_url` VARCHAR(8192) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`username` VARCHAR(128) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`password` VARCHAR(1024) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`email` VARCHAR(1024) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`email_is_verified` BIT(1) NOT NULL DEFAULT b'0',
+	`password_reset_is_required` BIT(1) NOT NULL DEFAULT b'0',
+	`account_is_locked` BIT(1) NOT NULL DEFAULT b'0',
+	`account_is_banned` BIT(1) NOT NULL DEFAULT b'0',
+	PRIMARY KEY (`id`) USING BTREE,
+	UNIQUE INDEX `username` (`username`) USING BTREE,
+	UNIQUE INDEX `email` (`email`) USING HASH
+)
+COLLATE='utf8mb4_unicode_ci'
+ENGINE=InnoDB;
