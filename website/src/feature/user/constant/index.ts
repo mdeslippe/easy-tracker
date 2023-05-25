70f19babec11f79d710e41c06e68fd304dd098ea
Myles Deslippe <mdeslippe76@gmail.com>
2023-05-25 09:56:54 -0400

    #73: Defined user constraint constants

---
+++ website/src/feature/user/constant/index.ts
@@ -0,0 +1,49 @@
+/**
+ * The minimum value that a user's id must be.
+ */
+export const USER_ID_MIN_VALUE = import.meta.env.VITE_USER_ID_MIN_VALUE;
+
+/**
+ * The maximum value that a user's id can be.
+ */
+export const USER_ID_MAX_VALUE = import.meta.env.VITE_USER_ID_MAX_VALUE;
+
+/**
+ * The minimum length a user's profile picture url must be.
+ */
+export const USER_PROFILE_URL_MIN_LENGTH = import.meta.env.VITE_USER_PROFILE_URL_MIN_LENGTH;
+
+/**
+ * The maximum length a user's profile picture url can be.
+ */
+export const USER_PROFILE_URL_MAX_LENGTH = import.meta.env.VITE_USER_PROFILE_URL_MAX_LENGTH;
+
+/**
+ * The minimum length a user's username must be.
+ */
+export const USER_USERNAME_MIN_LENGTH = import.meta.env.VITE_USER_USERNAME_MIN_LENGTH;
+
+/**
+ * The maximum length a user's username can be.
+ */
+export const USER_USERNAME_MAX_LENGTH = import.meta.env.VITE_USER_USERNAME_MAX_LENGTH;
+
+/**
+ * The minimum length a user's password must be.
+ */
+export const USER_PASSWORD_MIN_LENGTH = import.meta.env.VITE_USER_PASSWORD_MIN_LENGTH;
+
+/**
+ * The maximum length a user's password can be.
+ */
+export const USER_PASSWORD_MAX_LENGTH = import.meta.env.VITE_USER_PASSWORD_MAX_LENGTH;
+
+/**
+ * The minimum length a user's email address must be.
+ */
+export const USER_EMAIL_MIN_LENGTH = import.meta.env.VITE_USER_EMAIL_MIN_LENGTH;
+
+/**
+ * The maximum length a user's email address can be.
+ */
+export const USER_EMAIL_MAX_LENGTH = import.meta.env.VITE_USER_EMAIL_MAX_LENGTH;
