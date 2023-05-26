f7ed85ca3dd71d4d509fa5ffe5672ecb932d2b3a
Myles Deslippe <mdeslippe76@gmail.com>
2023-05-26 08:57:01 -0400

    #73: Added request and response types and schemas for user requests

--- website/src/feature/user/model/schemas.ts
+++ website/src/feature/user/model/schemas.ts
@@ -1,11 +1,17 @@
 // Zod.
 import { z } from 'zod';
 
+// Schemas.
+import { ValidationErrorResponseSchema } from '@website/common/model';
+
 // Constants.
 import {
 	USER_EMAIL_MAX_LENGTH,
 	USER_EMAIL_MIN_LENGTH,
+	USER_ID_MAX_VALUE,
 	USER_ID_MIN_VALUE,
+	USER_PASSWORD_MAX_LENGTH,
+	USER_PASSWORD_MIN_LENGTH,
 	USER_PROFILE_URL_MAX_LENGTH,
 	USER_PROFILE_URL_MIN_LENGTH,
 	USER_USERNAME_MAX_LENGTH,
@@ -19,17 +25,17 @@ export const UserSchema = z.object({
 	/**
 	 * The user's unique identifier.
 	 */
-	id: z.number().int().min(USER_ID_MIN_VALUE).max(USER_ID_MIN_VALUE),
+	id: z.number().int().min(USER_ID_MIN_VALUE).max(USER_ID_MAX_VALUE),
 
 	/**
 	 * The date and time the user's account was created at.
 	 */
-	account_created_at: z.coerce.date(),
+	accountCreatedAt: z.coerce.date(),
 
 	/**
 	 * A url to the user's profile picture.
 	 */
-	profile_picture_url: z
+	profilePictureUrl: z
 		.string()
 		.url()
 		.min(USER_PROFILE_URL_MIN_LENGTH)
@@ -48,20 +54,145 @@ export const UserSchema = z.object({
 	/**
 	 * If the user has verified their email address.
 	 */
-	email_is_verified: z.boolean(),
+	emailIsVerified: z.boolean(),
 
 	/**
 	 * If the user is required to reset their password before they can login.
 	 */
-	password_reset_is_required: z.boolean(),
+	passwordResetIsRequired: z.boolean(),
 
 	/**
 	 * If the user's account has been locked.
 	 */
-	account_is_locked: z.boolean(),
+	accountIsLocked: z.boolean(),
 
 	/**
 	 * If the user's account has been banned.
 	 */
-	account_is_banned: z.boolean()
+	accountIsBanned: z.boolean()
 });
+
+/**
+ * A schema for request data for requests to create a user.
+ */
+export const CreateUserRequestDataSchema = z.object({
+	username: z.string().min(USER_USERNAME_MIN_LENGTH).max(USER_USERNAME_MAX_LENGTH),
+	password: z.string().min(USER_PASSWORD_MIN_LENGTH).max(USER_PASSWORD_MAX_LENGTH),
+	email: z.string().email().min(USER_EMAIL_MIN_LENGTH).max(USER_EMAIL_MAX_LENGTH)
+});
+
+/**
+ * A schema for response data for requests to create a user.
+ */
+export const CreateUserResponseDataSchema = z.union([
+	UserSchema,
+	ValidationErrorResponseSchema,
+	z.undefined()
+]);
+
+/**
+ * A schema for request data for requests to get a user by id.
+ */
+export const GetUserByIDRequestDataSchema = z
+	.number()
+	.int()
+	.min(USER_ID_MIN_VALUE)
+	.max(USER_ID_MAX_VALUE);
+
+/**
+ * A schema for response data for requests to get a user by id.
+ */
+export const GetUserByIDResponseDataSchema = z.union([
+	z.object({
+		id: z.number().int().min(USER_ID_MIN_VALUE).max(USER_ID_MAX_VALUE),
+		accountCreatedAt: z.coerce.date(),
+		profilePictureUrl: z
+			.string()
+			.url()
+			.min(USER_PROFILE_URL_MIN_LENGTH)
+			.max(USER_PROFILE_URL_MAX_LENGTH),
+		username: z.string().min(USER_USERNAME_MIN_LENGTH).max(USER_USERNAME_MAX_LENGTH)
+	}),
+	z.undefined()
+]);
+
+/**
+ * A schema for request data for requests to get a user by username.
+ */
+export const GetUserByUsernameRequestDataSchema = z
+	.string()
+	.min(USER_USERNAME_MIN_LENGTH)
+	.max(USER_USERNAME_MAX_LENGTH);
+
+/**
+ * A schema for response data for requests to get a user by username.
+ */
+export const GetUserByUsernameResponseDataSchema = z.union([
+	z.object({
+		id: z.number().int().min(USER_ID_MIN_VALUE).max(USER_ID_MAX_VALUE),
+		accountCreatedAt: z.coerce.date(),
+		profilePictureUrl: z
+			.string()
+			.url()
+			.min(USER_PROFILE_URL_MIN_LENGTH)
+			.max(USER_PROFILE_URL_MAX_LENGTH),
+		username: z.string().min(USER_USERNAME_MIN_LENGTH).max(USER_USERNAME_MAX_LENGTH)
+	}),
+	z.undefined()
+]);
+
+/**
+ * A schema for request data for requests to get a user by email address.
+ */
+export const GetUserByEmailRequestDataSchema = z
+	.string()
+	.email()
+	.min(USER_EMAIL_MIN_LENGTH)
+	.max(USER_EMAIL_MAX_LENGTH);
+
+/**
+ * A schema for response data for requests to get a user by email address.
+ */
+export const GetUserByEmailResponseDataSchema = z.union([
+	z.object({
+		id: z.number().int().min(USER_ID_MIN_VALUE).max(USER_ID_MAX_VALUE),
+		accountCreatedAt: z.coerce.date(),
+		profilePictureUrl: z.string(),
+		username: z.string().min(USER_USERNAME_MIN_LENGTH).max(USER_USERNAME_MAX_LENGTH)
+	}),
+	z.undefined()
+]);
+
+/**
+ * A schema for request data for requests to update a user.
+ */
+export const UpdateUserRequestDataSchema = z.object({
+	profilePictureUrl: z
+		.string()
+		.url()
+		.min(USER_PROFILE_URL_MIN_LENGTH)
+		.max(USER_PROFILE_URL_MAX_LENGTH)
+		.optional(),
+	username: z.string().min(USER_USERNAME_MIN_LENGTH).max(USER_USERNAME_MAX_LENGTH).optional(),
+	password: z.string().min(USER_PASSWORD_MIN_LENGTH).max(USER_PASSWORD_MAX_LENGTH).optional(),
+	email: z.string().email().min(USER_EMAIL_MIN_LENGTH).max(USER_EMAIL_MAX_LENGTH).optional()
+});
+
+/**
+ * A schema for response data for requests to update a user.
+ */
+export const UpdateUserResponseDataSchema = z.union([
+	UserSchema,
+	ValidationErrorResponseSchema,
+	z.undefined()
+]);
+
+/**
+ * A schema for request data for requests to delete a user.
+ */
+export const DeleteUserRequestDataSchema = z.undefined();
+
+/**
+ * A schema for response data for requests to delete a user.
+ */
+export const DeleteUserResponseDataSchema = z.undefined();
--- website/src/feature/user/model/types.ts
+++ website/src/feature/user/model/types.ts
@@ -1,3 +1,5 @@
+import { ValidationErrorResponse } from '@website/common/model';
+
 /**
  * A user type.
  */
@@ -10,12 +12,12 @@ export type User = {
 	/**
 	 * The date and time the user's account was created at.
 	 */
-	account_created_at: Date;
+	accountCreatedAt: Date;
 
 	/**
 	 * A url to the user's profile picture.
 	 */
-	profile_picture_url: string;
+	profilePictureUrl: string;
 
 	/**
 	 * The user's username.
@@ -30,20 +32,181 @@ export type User = {
 	/**
 	 * If the user has verified their email address.
 	 */
-	email_is_verified: boolean;
+	emailIsVerified: boolean;
 
 	/**
 	 * If the user is required to reset their password before they can login.
 	 */
-	password_reset_is_required: boolean;
+	passwordResetIsRequired: boolean;
 
 	/**
 	 * If the user's account has been locked.
 	 */
-	account_is_locked: boolean;
+	accountIsLocked: boolean;
 
 	/**
 	 * If the user's account has been banned.
 	 */
-	account_is_banned: boolean;
+	accountIsBanned: boolean;
 };
+
+/**
+ * The request data for requests to create a user.
+ */
+export type CreateUserRequestData = {
+	/**
+	 * The user's username.
+	 */
+	username: string;
+
+	/**
+	 * The user's password.
+	 */
+	password: string;
+
+	/**
+	 * The user's email address.
+	 */
+	email: string;
+};
+
+/**
+ * The response data for requests to create a user.
+ */
+export type CreateUserResponseData = User | ValidationErrorResponse | undefined;
+
+/**
+ * The request data for requests to get a user by id.
+ */
+export type GetUserByIDRequestData = number;
+
+/**
+ * The response data for requests to get a user by id.
+ */
+export type GetUserByIDResponseData =
+	| {
+			/**
+			 * The user's unique identifier.
+			 */
+			id: number;
+
+			/**
+			 * The date and time the user's account was created at.
+			 */
+			accountCreatedAt: Date;
+
+			/**
+			 * A url to the user's profile picture.
+			 */
+			profilePictureUrl: string;
+
+			/**
+			 * The user's username.
+			 */
+			username: string;
+	  }
+	| undefined;
+
+/**
+ * The request data for requests to get a user by username.
+ */
+export type GetUserByUsernameRequestData = string;
+
+/**
+ * The response data for requests to get a user by username.
+ */
+export type GetUserByUsernameResponseData =
+	| {
+			/**
+			 * The user's unique identifier.
+			 */
+			id: number;
+
+			/**
+			 * The date and time the user's account was created at.
+			 */
+			accountCreatedAt: Date;
+
+			/**
+			 * A url to the user's profile picture.
+			 */
+			profilePictureUrl: string;
+
+			/**
+			 * The user's username.
+			 */
+			username: string;
+	  }
+	| undefined;
+
+/**
+ * The request data for requests to get a user by email address.
+ */
+export type GetUserByEmailRequestData = string;
+
+/**
+ * The response data for requests to get a user by email address.
+ */
+export type GetUserByEmailResponseData =
+	| {
+			/**
+			 * The user's unique identifier.
+			 */
+			id: number;
+
+			/**
+			 * The date and time the user's account was created at.
+			 */
+			accountCreatedAt: Date;
+
+			/**
+			 * A url to the user's profile picture.
+			 */
+			profilePictureUrl: string;
+
+			/**
+			 * The user's username.
+			 */
+			username: string;
+	  }
+	| undefined;
+
+/**
+ * The request data for requests to update a user.
+ */
+export type UpdateUserRequestData = {
+	/**
+	 * A url to the user's profile picture.
+	 */
+	profilePictureUrl?: string;
+
+	/**
+	 * The user's username.
+	 */
+	username?: string;
+
+	/**
+	 * The user's password.
+	 */
+	password?: string;
+
+	/**
+	 * The user's email address.
+	 */
+	email?: string;
+};
+
+/**
+ * The response data for requests to update a user.
+ */
+export type UpdateUserResponseData = User | ValidationErrorResponse | undefined;
+
+/**
+ * The request data for requests to delete a user.
+ */
+export type DeleteUserRequestData = undefined;
+
+/**
+ * The response data for requests to delete a user.
+ */
+export type DeleteUserResponseData = undefined;
