3704f4dd8392080f41a3304a694a1953478f78bd
Myles Deslippe <mdeslippe76@gmail.com>
2023-05-27 13:29:27 -0400

    #80: Added a landing page

---
+++ website/src/page/LandingPage/LandingPage.tsx
@@ -0,0 +1,11 @@
+// React.
+import { Fragment } from 'react';
+
+/**
+ * A landing page component.
+ *
+ * @returns The landing page.
+ */
+export function LandingPage(): JSX.Element {
+	return <Fragment></Fragment>;
+}
---
+++ website/src/page/LandingPage/index.ts
@@ -0,0 +1,1 @@
+export * from '@website/page/LandingPage/LandingPage';
