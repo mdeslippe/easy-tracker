bcaf5f7ee32afadddeebf9b38177d25f745199d0
Myles Deslippe <mdeslippe76@gmail.com>
2023-06-05 08:36:09 -0400

    #103: Implemented the login page

--- website/src/page/LoginPage/LoginPage.tsx
+++ website/src/page/LoginPage/LoginPage.tsx
@@ -2,7 +2,11 @@
 import { Fragment } from 'react';
 
 // Custom.
+import { LoginForm } from '@website/feature/auth/component';
 import { TopNavigationBar } from '@website/common/component/shared';
+
+// CSS.
+import '@website/page/LoginPage/loginPage.css';
 
 /**
  * A login page component.
@@ -13,6 +17,32 @@ export function LoginPage(): JSX.Element {
 	return (
 		<Fragment>
 			<TopNavigationBar />
+			<Main />
 		</Fragment>
+	);
+}
+
+/**
+ * The main content for the login page.
+ *
+ * @returns The main content for the login page.
+ */
+function Main(): JSX.Element {
+	return (
+		<main id='login'>
+			<div>
+				<div className='login-welcome-container'>
+					<img
+						className='hide-medium no-select'
+						src='/images/pictures/code.svg'
+						alt=''
+					/>
+					<h1>Welcome back!</h1>
+				</div>
+				<div className='login-form-wrapper'>
+					<LoginForm />
+				</div>
+			</div>
+		</main>
 	);
 }
