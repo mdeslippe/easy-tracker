1c0b5e3605c176f140ea099ec2ec42dd34f17490
Myles Deslippe <mdeslippe76@gmail.com>
2023-06-06 11:17:06 -0400

    #121: The home page will now be displayed if the user is authenticated

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -8,12 +8,18 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+// Hooks.
+import { useAuthenticatedUser } from '@website/feature/auth/hook';
+
 // Custom.
 import { LoadingOverlay } from '@website/common/component/display';
 
 // Pages.
 const LandingPage = lazy(() =>
 	import('@website/page').then((module) => ({ default: module.LandingPage }))
+);
+const HomePage = lazy(() =>
+	import('@website/page').then((module) => ({ default: module.HomePage }))
 );
 const LoginPage = lazy(() =>
 	import('@website/page').then((module) => ({ default: module.LoginPage }))
@@ -34,25 +40,38 @@ function App(): JSX.Element {
 	return (
 		<Suspense fallback={<LoadingOverlay />}>
 			<QueryClientProvider client={queryClient}>
-				<BrowserRouter>
-					<Routes>
-						<Route
-							path='/'
-							Component={LandingPage}
-						/>
-						<Route
-							path='/signup'
-							Component={SignUpPage}
-						/>
-						<Route
-							path='/login'
-							Component={LoginPage}
-						/>
-					</Routes>
-				</BrowserRouter>
+				<Router />
 				<ReactQueryDevtools initialIsOpen={false} />
 			</QueryClientProvider>
 		</Suspense>
+	);
+}
+
+/**
+ * The router for the application.
+ *
+ * @returns The router.
+ */
+function Router(): JSX.Element {
+	const { isAuthenticated } = useAuthenticatedUser();
+
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route
+					path='/'
+					Component={isAuthenticated ? HomePage : LandingPage}
+				/>
+				<Route
+					path='/signup'
+					Component={SignUpPage}
+				/>
+				<Route
+					path='/login'
+					Component={LoginPage}
+				/>
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
