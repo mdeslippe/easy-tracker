74358b22a8d4c2b93b43c8164099c87356586690
Myles Deslippe <mdeslippe76@gmail.com>
2023-06-09 09:06:41 -0400

    #127: Added test cases for the logout page

---
+++ website/src/page/LogoutPage/LogoutPage.test.tsx
@@ -0,0 +1,36 @@
+// Vitest.
+import { describe, it } from 'vitest';
+
+// React router.
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+// React testing library.
+import { render } from '@testing-library/react';
+
+// React query.
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// Pages.
+import { LogoutPage } from '@website/page/LogoutPage/LogoutPage';
+
+// Test cases for rendering the logout page component.
+describe('Logout Page component rendering', () => {
+	it('Renders the logout page component without crashing', () => {
+		// Create a react query client.
+		const queryClient = new QueryClient();
+
+		// Render the component.
+		render(
+			<QueryClientProvider client={queryClient}>
+				<BrowserRouter>
+					<Routes>
+						<Route
+							path='/'
+							element={<LogoutPage />}
+						/>
+					</Routes>
+				</BrowserRouter>
+			</QueryClientProvider>
+		);
+	});
+});
