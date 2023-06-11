280a4043fd07268502fddd45287d3959709b59fa
Myles Deslippe <mdeslippe76@gmail.com>
2023-06-11 11:48:04 -0400

    #138: Added a test case for the error page

---
+++ website/src/page/ErrorPage/ErrorPage.test.tsx
@@ -0,0 +1,28 @@
+// Vitest.
+import { describe, it } from 'vitest';
+
+// React router.
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+// React testing library.
+import { render } from '@testing-library/react';
+
+// Pages.
+import { ErrorPage } from '@website/page/ErrorPage/ErrorPage';
+
+// Test cases for rendering the error page component.
+describe('Error Page component rendering', () => {
+	it('Renders the error page component without crashing', () => {
+		// Render the component.
+		render(
+			<BrowserRouter>
+				<Routes>
+					<Route
+						path='/'
+						element={<ErrorPage />}
+					/>
+				</Routes>
+			</BrowserRouter>
+		);
+	});
+});
