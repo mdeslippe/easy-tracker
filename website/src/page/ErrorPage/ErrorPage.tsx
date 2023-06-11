6a9998b831d887703658e086d012e46c09282a05
Myles Deslippe <mdeslippe76@gmail.com>
2023-06-11 15:09:34 -0400

    #138: Added content to the error page

--- website/src/page/ErrorPage/ErrorPage.tsx
+++ website/src/page/ErrorPage/ErrorPage.tsx
@@ -23,5 +23,23 @@ export function ErrorPage(): JSX.Element {
  * @returns The main content for the error page.
  */
 function Main(): JSX.Element {
-	return <main></main>;
+	return (
+		<main id='error'>
+			<div>
+				<div className='error-message-container'>
+					<h1>Oops...</h1>
+					<p>
+						It looks like an unexpected error has occurred, please try again later. We
+						apologize for any inconvenience.
+					</p>
+				</div>
+				<div className='error-picture-container'>
+					<img
+						src='/images/pictures/error-500.svg'
+						alt=''
+					/>
+				</div>
+			</div>
+		</main>
+	);
 }
