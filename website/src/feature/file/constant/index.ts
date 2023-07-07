60457eab6010700d8f17004743ab8ac35fe0618b
Myles Deslippe <mdeslippe76@gmail.com>
2023-07-07 10:20:58 -0400

    #181: Added file validation constants

---
+++ website/src/feature/file/constant/index.ts
@@ -0,0 +1,49 @@
+/**
+ * The minimum value that a file's id must be.
+ */
+export const FILE_ID_MIN_VALUE = import.meta.env.VITE_FILE_ID_MIN_VALUE;
+
+/**
+ * The maximum value that a file's id can be.
+ */
+export const FILE_ID_MAX_VALUE = import.meta.env.VITE_FILE_ID_MAX_VALUE;
+
+/**
+ * The minimum value that a file user's id must be.
+ */
+export const FILE_USER_ID_MIN_VALUE = import.meta.env.VITE_FILE_USER_ID_MIN_VALUE;
+
+/**
+ * The maximum value that a file user's id can be.
+ */
+export const FILE_USER_ID_MAX_VALUE = import.meta.env.VITE_FILE_USER_ID_MAX_VALUE;
+
+/**
+ * The minimum length that a file's mime type must be.
+ */
+export const FILE_MIME_TYPE_MIN_LENGTH = import.meta.env.VITE_FILE_MIME_TYPE_MIN_LENGTH;
+
+/**
+ * The maximum length that a file's mime type can be.
+ */
+export const FILE_MIME_TYPE_MAX_LENGTH = import.meta.env.VITE_FILE_MIME_TYPE_MAX_LENGTH;
+
+/**
+ * The minimum length that a file's name must be.
+ */
+export const FILE_NAME_MIN_LENGTH = import.meta.env.VITE_FILE_NAME_MIN_LENGTH;
+
+/**
+ * The maximum length that a file's name can be.
+ */
+export const FILE_NAME_MAX_LENGTH = import.meta.env.VITE_FILE_NAME_MAX_LENGTH;
+
+/**
+ * The minimum length that a file's data must be.
+ */
+export const FILE_DATA_MIN_LENGTH = import.meta.env.VITE_FILE_DATA_MIN_LENGTH;
+
+/**
+ * The maximum length that a file's data can be.
+ */
+export const FILE_DATA_MAX_LENGTH = import.meta.env.VITE_FILE_DATA_MAX_LENGTH;
