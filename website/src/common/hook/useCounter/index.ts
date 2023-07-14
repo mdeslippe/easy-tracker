0b658cf0776e5148f4d12a3d86eac1f8167e2a1b
Myles Deslippe <mdeslippe76@gmail.com>
2023-07-14 16:24:14 -0400

    #117: Added a return type to the counter hook

--- website/src/common/hook/useCounter/index.ts
+++ website/src/common/hook/useCounter/index.ts
@@ -2,12 +2,17 @@
 import { useReducer } from 'react';
 
 /**
+ * The data that will be return from the useCounter hook.
+ */
+export type UseCounterResult = () => number;
+
+/**
  * A hook that can be used to get integers in an ascending sequence.
  *
  * @param initialValue The initial value of the counter.
  * @returns A function that retrieves the next count.
  */
-export function useCounter(initialValue: number = 0) {
+export function useCounter(initialValue: number = 0): UseCounterResult {
 	const [count, nextCount] = useReducer<(count: number) => number>(
 		(count) => count + 1,
 		initialValue
