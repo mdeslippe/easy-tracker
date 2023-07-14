073d0b405f29eae52375b4d3bcf10cf568ed6cb4
Myles Deslippe <mdeslippe76@gmail.com>
2023-07-14 13:57:29 -0400

    #117: Created a SnackBarContext that can be used to store snacks

---
+++ website/src/common/context/SnackBarContext/index.ts
@@ -0,0 +1,60 @@
+// React.
+import { Context, createContext } from 'react';
+
+/**
+ * A snack that can be displayed in a snack bar.
+ */
+export type Snack = {
+	/**
+	 * A unique identifier that was generated for the snack.
+	 */
+	readonly id: string;
+
+	/**
+	 * The type of snack.
+	 */
+	readonly type: 'success' | 'warning' | 'error';
+
+	/**
+	 * The duration in milliseconds that the snack will be displayed for.
+	 */
+	readonly duration: number;
+
+	/**
+	 * The message that the snack will display.
+	 */
+	readonly message: string;
+};
+
+/**
+ * The data that is stored in the {@link SnackBarContext}.
+ */
+export type SnackBarContextData = {
+	/**
+	 * The snacks that are currently in the snack bar.
+	 */
+	readonly snacks: Array<Snack>;
+
+	/**
+	 * A function that can be used to add a snack to the snack bar.
+	 *
+	 * @param snack The snack to add to the snack bar.
+	 */
+	readonly addSnack: (snack: Omit<Snack, 'id'>) => void;
+
+	/**
+	 * A function that can be used to remove a snack from the snack bar.
+	 *
+	 * @param id The id of the snack to remove from the snack bar.
+	 */
+	readonly removeSnack: (id: string) => void;
+};
+
+/**
+ * The snack bar context.
+ */
+export const SnackBarContext: Context<SnackBarContextData> = createContext<SnackBarContextData>({
+	snacks: new Array<Snack>(),
+	addSnack: () => {},
+	removeSnack: () => {}
+});
