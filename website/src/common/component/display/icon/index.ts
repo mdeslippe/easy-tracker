1f2e0da88c14feaf0a1d286124cb46991826ae44
Myles Deslippe <mdeslippe76@gmail.com>
2023-07-18 09:56:46 -0400

    #203: Added a close icon component

---
+++ website/src/common/component/display/icon/index.ts
@@ -0,0 +1,21 @@
+export * from '@website/common/component/display/icon/CloseIcon';
+
+/**
+ * An enumeration of the possible icons sizes.
+ */
+export enum IconSize {
+	/**
+	 * A large icon size.
+	 */
+	LARGE = 'large',
+
+	/**
+	 * A medium icon size.
+	 */
+	MEDIUM = 'medium',
+
+	/**
+	 * A small icon size.
+	 */
+	SMALL = 'small'
+}
--- website/src/common/component/display/index.ts
+++ website/src/common/component/display/index.ts
@@ -1,3 +1,4 @@
+export * from '@website/common/component/display/icon';
 export * from '@website/common/component/display/Spinner';
 export * from '@website/common/component/display/Skeleton';
 export * from '@website/common/component/display/ErrorBox';
