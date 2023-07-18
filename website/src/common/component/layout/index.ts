b6c3b9793b57d1bbd2d59cd00fcc7c3be481f0fb
Myles Deslippe <mdeslippe76@gmail.com>
2023-07-18 10:16:53 -0400

    #203: Added a modal component

--- website/src/common/component/layout/index.ts
+++ website/src/common/component/layout/index.ts
@@ -1,1 +1,2 @@
+export * from '@website/common/component/layout/modal';
 export * from '@website/common/component/layout/MaxWidthContainer';
---
+++ website/src/common/component/layout/modal/Modal/Modal.tsx
@@ -0,0 +1,78 @@
+// React.
+import { ComponentProps, useEffect, useRef } from 'react';
+
+// CSS.
+import '@website/common/component/layout/modal/Modal/modal.css';
+
+/**
+ * Properties for the {@link Modal} component.
+ */
+export interface ModalProps extends ComponentProps<'dialog'> {
+	/**
+	 * If the modal is open.
+	 */
+	open: boolean;
+
+	/**
+	 * A function that will be invoked when the modal closes.
+	 */
+	onClose: () => void;
+}
+
+/**
+ * A modal component.
+ *
+ * @param props The component's properties.
+ * @returns The modal.
+ */
+export function Modal({
+	className,
+	children,
+	open,
+	onClose,
+	onClick,
+	...props
+}: ModalProps): JSX.Element {
+	const ref = useRef<HTMLDialogElement | null>(null);
+
+	useEffect(() => {
+		// If the ref is null.
+		if (ref.current === null) {
+			return;
+		}
+
+		// If the modal should be open, but it is closed.
+		if (open && !ref.current.open) {
+			ref.current.showModal();
+		}
+
+		// If the modal should be closed, but it is open.
+		if (!open && ref.current.open) {
+			ref.current.close();
+		}
+	}, [open, ref.current, ref.current?.open]);
+
+	return (
+		<dialog
+			{...props}
+			ref={ref}
+			className={className ? `${className} modal` : 'modal'}
+			onClose={onClose}
+			onClick={(event) => {
+				// If the user clicked outside of the modal's content, the modal should be closed.
+				if (event?.target instanceof HTMLDialogElement) {
+					onClose();
+					return;
+				}
+
+				// If a click event handler has been passed in, propagate the click event.
+				if (onClick !== undefined) {
+					onClick(event);
+					return;
+				}
+			}}
+		>
+			{children}
+		</dialog>
+	);
+}
