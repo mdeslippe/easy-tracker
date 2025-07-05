1b5dda8f78c218eeab30c17318cc8d19d3829fe6
Myles Deslippe <mdeslippe76@gmail.com>
2025-07-05 11:40:44 -0400

    #241: Fixed the file data length validation data type

--- server/src/feature/file/model/mod.rs
+++ server/src/feature/file/model/mod.rs
@@ -25,7 +25,7 @@ pub(crate) struct File {
     pub(crate) name: String,
 
     /// The file's raw data.
-    #[validate(length(max = 4294967295))]
+    #[validate(length(max = 4294967295u64))]
     pub(crate) data: Vec<u8>,
 }
 
